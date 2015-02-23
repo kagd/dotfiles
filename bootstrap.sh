56c232f6bde967a7a5ff30dfec5643842a8dbdbf
Grant Klinsing <grant@digitalopera.com>
2015-02-23 09:38:59 -0700

    Remove sublime packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "sublime-snippets/" --exclude "README.md" -av . ~
-  rsync -r --include '*.sublime-snippet' --include '*.sublime-settings' sublime/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
