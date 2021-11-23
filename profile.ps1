1732f12bdfabbda3147df89c5a98bc0ee76c3050
Grant Klinsing <gklinsing@gmail.com>
2021-11-23 09:29:41 -0600

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,63 @@
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+#################################
+# git-posh
+#################################
+Import-Module 'C:\Users\klingra\Code\posh-git\src\posh-git.psd1'
+$GitPromptSettings.DefaultPromptPath.ForegroundColor = 0xFFA500
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+
+#################################
+# kubebtl
+#################################
+$Env:kubectlEnv = 'dev';
+
+function k {
+    kubectl $args
+}
+
+function kenv {
+    $Env:kubectlEnv = $args[0];
+}
+
+# ex: kpods dev [extra args]
+function kpods {
+    $env = getKubectlEnv;
+    $namespace = "${env}-online";
+    $rest = $args -join ' ';
+    # "-o wide" = print with more info
+    kubectlEchoAndExec get pods --namespace $namespace -o wide $rest
+}
+
+# kubectl logs --namespace prd-online online-orderaccept-consumer-75cbcd79dc-5grsk -c online-orderaccept-consumer
+function klogs {
+    $env = getKubectlEnv;
+    $namespace = "${env}-online";
+    $rest = $args -join ' ';
+    # "-o wide" = print with more info
+    kubectlEchoAndExec logs --namespace $namespace $rest
+}
+
+# echo command and exec it
+function kubectlEchoAndExec {
+    $str = $args -join ' ';
+    Write-Output "$ kubectl ${str}";
+    kubectl $args;
+}
+
+# get user iinput value or default
+function getKubectlEnv {
+    $defaultEnv = $Env:kubectlEnv;
+    $val = Read-Host -Prompt "What env? (Default is '${defaultEnv}')";
+    
+    # return default if null or empty
+    if($null -eq $val){
+        return $defaultEnv;
+    }
+    else {
+        $trimmed = $val -replace '\s', '';
+        if($trimmed -eq ''){
+            return $defaultEnv;
+        }
+    }
+
+    return $val;
+}
